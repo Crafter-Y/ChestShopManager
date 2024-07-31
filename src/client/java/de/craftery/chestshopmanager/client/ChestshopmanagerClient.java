8a34745f2da8263d75f6693c58edc544361dee8e
Crafter-Y <mrcrafter.yt1@gmail.com>
2024-07-31 17:16:27 +0200

    Ready. Set. GO!

---
+++ src/client/java/de/craftery/chestshopmanager/client/ChestshopmanagerClient.java
@@ -0,0 +1,10 @@
+package de.craftery.chestshopmanager.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChestshopmanagerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/de/craftery/chestshopmanager/Chestshopmanager.java
@@ -0,0 +1,10 @@
+package de.craftery.chestshopmanager;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Chestshopmanager implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
